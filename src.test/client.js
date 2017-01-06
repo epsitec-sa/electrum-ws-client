26b2304783ced7611690439acdf49cbbe47910b6
Pierre Arnaud <arnaud@epsitec.ch>
2017-01-06 06:48:57 +0100

    Add empty test and library

---
+++ src.test/client.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import {expect} from 'mai-chai';
+
+/******************************************************************************/
+
+describe ('Client', () => {
+  it ('Does nothing', () => {
+    expect (true).to.be.true ();
+  });
+});
