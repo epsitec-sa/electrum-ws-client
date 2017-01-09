1c38af5b026086c30770f82db22a1f630ae889ab
Pierre Arnaud <arnaud@epsitec.ch>
2017-01-09 06:22:54 +0100

    Add receive() and full tests

--- src.test/web-socket-channel.js
+++ src.test/web-socket-channel.js
@@ -3,45 +3,88 @@
 import {expect} from 'mai-chai';
 import {WebSocketChannel} from 'electrum-ws-client';
 
-import all from 'mai-chai';
+/******************************************************************************/
 
-console.log (all.chaiAsPromised);
+function delay (ms) {
+  return new Promise (function (resolve, reject) {
+      setTimeout (resolve, ms);
+    });
+}
 
 /******************************************************************************/
 
 describe ('WebSocketChannel', () => {
   describe ('Constructor', () => {
     it ('builds URI, no arguments provided', () => {
-      var channel = new WebSocketChannel ();
+      const channel = new WebSocketChannel ();
       expect (channel.uri).to.equal ('ws://localhost:80/');
     });
     it ('builds URI, server and port provided', () => {
-      var channel = new WebSocketChannel ('example.org', 1234);
+      const channel = new WebSocketChannel ('example.org', 1234);
       expect (channel.uri).to.equal ('ws://example.org:1234/');
     });
     it ('builds URI, server, port and path provided', () => {
-      var channel = new WebSocketChannel ('example.org', 1234, 'echo');
+      const channel = new WebSocketChannel ('example.org', 1234, 'echo');
       expect (channel.uri).to.equal ('ws://example.org:1234/echo');
     });
   });
 
   describe ('open() on unknown URL', ()=> {
     it ('throws an exception', () => {
-      var channel = new WebSocketChannel ('localhost', 12345);
+      const channel = new WebSocketChannel ('localhost', 12345);
       return expect (channel.open ()).to.eventually.be.rejected;
     });
   });
 
   describe ('open() on listening URL', ()=> {
     it ('connects to WS server', () => {
-      var channel = new WebSocketChannel ('localhost', 54321);
+      const channel = new WebSocketChannel ('localhost', 54321);
       return expect (channel.open ()).to.eventually.equal ('connected');
     });
-    it ('send() then works', async () => {
-      var channel = new WebSocketChannel ('localhost', 54321);
-      var result = await channel.open ();
+  });
+
+  describe ('send() on open channel', ()=> {
+    it ('works', async () => {
+      const channel = new WebSocketChannel ('localhost', 54321);
+      const result = await channel.open ();
       expect (result).to.equal ('connected');
       channel.send ({foo: 42});
     });
   });
+
+  describe ('receive() after message is received', ()=> {
+    it ('gets echo back', async () => {
+      const channel = new WebSocketChannel ('localhost', 54321);
+      const result = await channel.open ();
+      expect (result).to.equal ('connected');
+      channel.send ({foo: 42});
+      expect (channel.receiveReadyCount).to.equal (0);
+      expect (channel.receiveWaitingCount).to.equal (0);
+      await delay (10);
+      expect (channel.receiveReadyCount).to.equal (1);
+      expect (channel.receiveWaitingCount).to.equal (0);
+      const echo = await channel.receive ();
+      expect (channel.receiveReadyCount).to.equal (0);
+      expect (channel.receiveWaitingCount).to.equal (0);
+      expect (echo).to.deep.equal ({foo: 42});
+    });
+  });
+
+  describe ('receive() before message is received', ()=> {
+    it ('gets echo back', async () => {
+      const channel = new WebSocketChannel ('localhost', 54321);
+      const result = await channel.open ();
+      const echoPromise = channel.receive ();
+      expect (result).to.equal ('connected');
+      channel.send ({foo: 42});
+      expect (channel.receiveReadyCount).to.equal (0);
+      expect (channel.receiveWaitingCount).to.equal (1);
+      const echo = await echoPromise;
+      expect (channel.receiveReadyCount).to.equal (0);
+      expect (channel.receiveWaitingCount).to.equal (0);
+      expect (echo).to.deep.equal ({foo: 42});
+    });
+  });
 });
+
+/******************************************************************************/
--- src/web-socket-channel.js
+++ src/web-socket-channel.js
@@ -2,13 +2,13 @@
 
 import WebSocket from 'ws';
 
+/******************************************************************************/
+
 export class WebSocketChannel {
   constructor(server = 'localhost', port = '80', path = '') {
     this._uri = `ws://${server}:${port}/${path}`;
-  }
-
-  get uri () {
-    return this._uri;
+    this._waits = [];
+    this._ready = [];
   }
 
   open () {
@@ -18,7 +18,7 @@ export class WebSocketChannel {
 
     return new Promise ((resolve, reject) => {
       this._ws = new WebSocket (this._uri);
-      this._ws.onmessage = event => this.processMessage (JSON.parse (event.data));
+      this._ws.onmessage = event => this._processMessage (JSON.parse (event.data));
       this._ws.onopen = event => resolve ('connected');
       this._ws.onerror = event => reject (event);
     });
@@ -29,14 +29,50 @@ export class WebSocketChannel {
       throw 'WebSocketChannel must be opened first';
     }
 
-    this._ws.send (JSON.stringify (obj));
+    return Promise.resolve (this._ws.send (JSON.stringify (obj)));
   }
 
-  get ws () {
-    return this._ws;
+  receive () {
+    if (this._ready.length > 0) {
+      // Return immediately the already received message
+      const value = this._ready.shift ();
+      return Promise.resolve (value);
+    } else {
+      // Queue a promise which will be resolved when we receive a message
+      const deferred = Promise.defer ();
+      this._waits.push (deferred);
+      return deferred.promise;
+    }
   }
 
-  processMessage (json) {
-    console.log (json);
+  /****************************************************************************/
+
+  get uri () {
+    return this._uri;
+  }
+
+  get receiveReadyCount () {
+    return this._ready.length;
+  }
+
+  get receiveWaitingCount () {
+    return this._waits.length;
+  }
+
+  /****************************************************************************/
+
+  _processMessage (json) {
+    if (this._waits.length == 0) {
+      // No receive is waiting for the message; enqueue it for future calls
+      // to receive()
+      this._ready.push (json);
+    } else {
+      // A receive() is blocked waiting for the message; resolve the pending
+      // promise by passing it the specified value
+      const deferred = this._waits.shift ();
+      deferred.resolve (json);
+    }
   }
 }
+
+/******************************************************************************/
