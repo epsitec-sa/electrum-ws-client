93cf9ff3ff054b02365feec951770b73e8b4d54f
Pierre Arnaud <arnaud@epsitec.ch>
2017-03-31 07:26:22 +0200

    Update babel and linting

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,10 +1,10 @@
-'use strict';
+/* global global console */
+/* eslint no-console: 0 */
 
 // Provide document and window as globals, so that React can be
 // tested outside of a browser environment.
 
 import jsdom from 'jsdom';
-import WebSocket from 'ws';
 
 const doc = jsdom.jsdom ('<!doctype html><html><body><div id="root"/></body></html>');
 const win = doc.defaultView;
@@ -14,11 +14,9 @@ const Chrome49 =
   'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)' +
   ' Chrome/49.0.2454.85 Safari/537.36';
 
-/* global global */
 global.document  = doc;
 global.window    = win;
 global.navigator = {userAgent: Chrome49};
-global.WebSocket = WebSocket;
 
 if (console.debug === undefined) {
   // Setting the userAgent to Chrome triggers a warning in React, which we
