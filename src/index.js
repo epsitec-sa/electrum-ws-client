5a31df4712385f0b9598bdf8d5697d73a9b21d64
Samuel Loup <loup@epsitec.ch>
2017-06-21 10:34:29 +0200

    Impl. node specific polyfills

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/* global global require */
+
+// NodeJS?
+if (typeof exports !== 'undefined') {
+  // Load babel polyfill
+  require ('regenerator-runtime/runtime');
+  // Inject WebSocket w3c compatible API
+  global.WebSocket = require ('websocket').w3cwebsocket;
+}
 export {WebSocketChannel} from './web-socket-channel.js';
 export {HubLoader} from './hub-loader.js';
 export {Correlator} from './correlator.js';
